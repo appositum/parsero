cc67b7c6adfa1d8cea5c70dea8339f4e39030538
eddie <appositum@protonmail.com>
2018-11-04 01:29:00 -0300

    Basic char parsers
    - space
    - lower
    - upper
    - alpha
    - alphaNum
    - string

--- src/Parsec.hs
+++ src/Parsec.hs
@@ -53,6 +53,28 @@ noneOf = satisfy . flip notElem
 char :: Char -> Parser Char
 char c = satisfy (==c)
 
+space :: Parser Char
+space = satisfy isSpace
+
+lower :: Parser Char
+lower = satisfy isLower
+
+upper :: Parser Char
+upper = satisfy isUpper
+
+alphaNum :: Parser Char
+alphaNum = satisfy isAlphaNum
+
+alpha :: Parser Char
+alpha = satisfy isAlpha
+
+letter :: Parser Char
+letter = alpha
+
+string :: String -> Parser String
+string "" = pure ""
+string ccs@(c:cs) = char c *> string cs *> pure ccs
+
 digit :: Parser Char
 digit = satisfy isDigit
 
