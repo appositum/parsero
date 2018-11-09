b86a7f448ea34b63b21b84d84955cbc687b9a50b
eddie <appositum@protonmail.com>
2018-11-08 23:41:35 -0300

    Export Tokenizer module

--- src/Text/Cuceta.hs
+++ src/Text/Cuceta.hs
@@ -2,8 +2,10 @@ module Text.Cuceta
   ( module Text.Cuceta.Char
   , module Text.Cuceta.Combinators
   , module Text.Cuceta.Parser
+  , module Text.Cuceta.Tokenizer
   ) where
 
 import Text.Cuceta.Char
 import Text.Cuceta.Combinators
 import Text.Cuceta.Parser
+import Text.Cuceta.Tokenizer
