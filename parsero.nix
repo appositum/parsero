5a3545d6d086b7575d1bb37c0a01e68040e9211b
eddie <appositum@protonmail.com>
2018-12-05 06:48:30 -0300

    Remove unused dependencies

--- parsero.nix
+++ parsero.nix
@@ -1,9 +1,9 @@
-{ mkDerivation, stdenv, base, hspec, QuickCheck, text, }:
+{ mkDerivation, stdenv, base, hspec, QuickCheck, text }:
 mkDerivation {
   pname = "parsero";
   version = "0.2.0.0";
   src = ./.;
-  libraryHaskellDepends = [ base hspec QuickCheck text ];
+  libraryHaskellDepends = [ base text ];
   testHaskellDepends = [ base hspec QuickCheck ];
   doHaddock = false;
   homepage = "https://github.com/appositum/parsero#readme";
