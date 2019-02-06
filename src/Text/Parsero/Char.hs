2e0382d3623f51474034156dc6c4bdab4976e980
eddie <appositum@protonmail.com>
2019-02-05 23:13:53 -0300

    MkParsero

--- src/Text/Parsero/Char.hs
+++ src/Text/Parsero/Char.hs
@@ -26,65 +26,65 @@ import           Text.Parsero.Parser
 import           Text.Parsero.Stream      (Stream)
 import qualified Text.Parsero.Stream      as S
 
-char :: Stream s => Char -> Parser s Char
+char :: Stream s => Char -> Parsero s Char
 char c = satisfy (==c)
 
-char' :: Stream s => Char -> Parser s Char
+char' :: Stream s => Char -> Parsero s Char
 char' c =  char (toLower c)
        <|> char (toUpper c)
        <|> char (toTitle c)
 
-anyChar :: Stream s => Parser s Char
+anyChar :: Stream s => Parsero s Char
 anyChar = satisfy (const True)
 
-notChar :: Stream s => Char -> Parser s Char
+notChar :: Stream s => Char -> Parsero s Char
 notChar c = satisfy (/=c)
 
-space :: Stream s => Parser s Char
+space :: Stream s => Parsero s Char
 space = satisfy isSpace
 
-skipWhitespaces :: Stream s => Parser s ()
+skipWhitespaces :: Stream s => Parsero s ()
 skipWhitespaces = skipMany space
 
-lower :: Stream s => Parser s Char
+lower :: Stream s => Parsero s Char
 lower = satisfy isLower
 
-upper :: Stream s => Parser s Char
+upper :: Stream s => Parsero s Char
 upper = satisfy isUpper
 
-alphaNum :: Stream s => Parser s Char
+alphaNum :: Stream s => Parsero s Char
 alphaNum = satisfy isAlphaNum
 
-alpha :: Stream s => Parser s Char
+alpha :: Stream s => Parsero s Char
 alpha = satisfy isAlpha
 
-letter :: Stream s => Parser s Char
+letter :: Stream s => Parsero s Char
 letter = alpha
 
-chunk :: Stream s => s -> Parser s s
+chunk :: Stream s => s -> Parsero s s
 chunk css
   | css == S.empty = pure S.empty
   | otherwise = char c *> chunk cs *> pure css
   where cs = S.tail css
         c = S.head css
 
-string :: Stream s => s -> Parser s s
+string :: Stream s => s -> Parsero s s
 string = chunk
 
-digit :: Stream s => Parser s Char
+digit :: Stream s => Parsero s Char
 digit = satisfy isDigit
 
-tab :: Stream s => Parser s Char
+tab :: Stream s => Parsero s Char
 tab = char '\t'
 
-newline :: Stream s => Parser s Char
+newline :: Stream s => Parsero s Char
 newline = char '\n'
 
-carriage :: Stream s => Parser s Char
+carriage :: Stream s => Parsero s Char
 carriage = char '\r'
 
-crlf :: Stream s => Parser s Char
+crlf :: Stream s => Parsero s Char
 crlf = carriage *> newline
 
-eol :: Stream s => Parser s Char
+eol :: Stream s => Parsero s Char
 eol = newline <|> crlf
