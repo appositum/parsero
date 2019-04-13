099041da1ee1f761c367924d130fea6d1c1853a4
eddie <appositum@protonmail.com>
2019-04-12 21:31:23 -0300

    Proper nix-shell environment

--- shell.nix
+++ shell.nix
@@ -1,14 +1,15 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "default", doBenchmark ? false }:
+with import <nixpkgs> {};
 
 let
-  inherit (nixpkgs) pkgs;
-
-  haskellPackages =
-    if compiler == "default"
-    then pkgs.haskellPackages
-    else pkgs.haskell.packages.${compiler};
-
-  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
-  drv = variant (haskellPackages.callPackage ./parsero.nix {});
+  env = haskellPackages.ghcWithPackages (p:
+    [
+      (p.callPackage ./parsero.nix {})
+    ]);
 in
-  if pkgs.lib.inNixShell then drv.env else drv
+stdenv.mkDerivation rec {
+  name = "env";
+  buildInputs = [ env ];
+  shellHook = ''
+  ghci && exit
+  '';
+}
